d5d5e1a6c530c3e895911d59a5e4df743dd449a2
Ted Anyansi <tanyansi@gmail.com>
2018-05-28 18:06:58 -0700

    configuring CORS config in Zuul main class in order to permit front-end to make api calls to the EC2 endpoint

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -20,15 +20,21 @@ public class ZuulGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
+
 	@Bean
 	public CorsFilter corsFilter() {
-
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		config.setAllowedMethods(Arrays.stream(Http.HttpMethod.values()).map(Http.HttpMethod::name).collect(Collectors.toList()));
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
