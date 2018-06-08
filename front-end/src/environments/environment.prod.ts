e1c08d8b5717cae8727f5f2238f74196bfcf67ff
Ted Anyansi <tanyansi@gmail.com>
2018-06-07 17:06:42 -0700

    centered the text for the 'click song to see analysis' prompt

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiHost: 'http://ec2-54-213-129-218.us-west-2.compute.amazonaws.com:8080',
-  nodeHost: 'http://ec2-54-213-129-218.us-west-2.compute.amazonaws.com:3001'
+  // nodeHost: 'http://ec2-54-213-129-218.us-west-2.compute.amazonaws.com:3001'
+  nodeHost: 'https://spotify-lit-node-server.herokuapp.com'
 };
--- songwiz-front-end/src/components/SongList.js
+++ songwiz-front-end/src/components/SongList.js
@@ -19,14 +19,16 @@ class SongList extends Component {
 
     render() {
       return (
-        <div className="App" className="songlist">
-        <p> <u> Click on a song below to see its analysis </u> </p>
-           {this.props.songs.map( (element, index) => {
-               console.log(element);
-               return (
-                <Song {...element} handleSongClick={this.props.handleSongClick} key={index} song={element}/>
-               );
-           })}
+        <div>
+          <p> <u> Click on a song below to see its analysis </u> </p>
+          <div className="App" className="songlist">
+            {this.props.songs.map( (element, index) => {
+                console.log(element);
+                return (
+                  <Song {...element} handleSongClick={this.props.handleSongClick} key={index} song={element}/>
+                );
+            })}
+          </div>
         </div>
       );
     }
